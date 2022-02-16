989735841f23bc22cc8f72c6c79f47da7320453f
Sadia-90 <sadiamahmood24@gmail.com>
2022-02-17 03:15:53 +0600

    3rd commi

---
+++ money.js
@@ -0,0 +1,24 @@
+const salaryBox = document.getElementById("salary-box");
+const foodBox = document.getElementById("Food-box");
+const rentBox = document.getElementById("Rent-box");
+const clothBox = document.getElementById("cloth-box");
+const expenseBox = document.getElementById("expenses-box");
+const savings = document.getElementById("percentage");
+
+const savingAmount = document.getElementById("Saving-box");
+const remainingBalance = document.getElementById("Remaining-box");
+
+
+
+
+
+
+
+
+
+const income = document.getElementById("salary-box").addEventListener('click', function () {
+
+
+
+
+})
